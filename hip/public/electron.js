8cb36dce85f7271b86ab8fcc66daedcc76e2d4b8
kayan9896 <a4ssgit@hotmail.com>
2024-12-02 16:28:30 -0800

    exe

---
+++ hip/public/electron.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+let isDev;
+import('electron-is-dev').then(module => {
+  isDev = module.default;
+});
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  // Load the index.html from a url
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
