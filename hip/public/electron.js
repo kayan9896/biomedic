003bbc13c1e7eb993fe3eaa517174942c8e39bc2
kayan9896 <90473306+kayan9896@users.noreply.github.com>
2024-12-03 18:46:46 +0000

    a

--- hip/public/electron.js
+++ hip/public/electron.js
@@ -1,11 +1,36 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
+const { spawn } = require('child_process');
 let isDev;
 import('electron-is-dev').then(module => {
   isDev = module.default;
 });
 
+let serverProcess;
+
+function startServer() {
+  const serverPath = isDev
+    ? path.join(__dirname, '../../dist/server.exe')
+    : path.join(process.resourcesPath, 'server.exe');
+
+  serverProcess = spawn(serverPath);
+
+  serverProcess.stdout.on('data', (data) => {
+    console.log(`Server stdout: ${data}`);
+  });
+
+  serverProcess.stderr.on('data', (data) => {
+    console.error(`Server stderr: ${data}`);
+  });
+
+  serverProcess.on('close', (code) => {
+    console.log(`Server process exited with code ${code}`);
+  });
+}
+
 function createWindow() {
+  startServer();
+
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
@@ -14,6 +39,7 @@ function createWindow() {
       nodeIntegration: true
     }
   });
+
   // Load the index.html from a url
   win.loadURL(
     isDev
@@ -23,11 +49,16 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
+
 app.on('window-all-closed', () => {
+  if (serverProcess) {
+    serverProcess.kill();
+  }
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
