cebf9817d2ffe8b05504b492a018178a59035855
kayan9896 <90473306+kayan9896@users.noreply.github.com>
2024-12-06 15:56:59 -0800

    Update App.js

--- hip/src/App.js
+++ hip/src/App.js
@@ -1,9 +1,15 @@
 import HipOperationSoftware from './HipOperationSoftware';
-
+function closewin(){
+  const remote=(window.require)?window.require('electron').remote:null;
+  const w=remote.getCurrentWindow()
+  w.close()
+}
 function App() {
+  
   return (
     <div className="App">
       <HipOperationSoftware />
+      <button onClick={closewin}>close</button>
     </div>
   );
 }
